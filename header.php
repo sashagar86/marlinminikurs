e4567fdf48750d27cb9881384e0981dccfc5c382
Alex Zhar <azharyokhin@intelliants.com>
2021-12-12 15:28:15 +0300

    Task 1 foreach

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8">
+        <title>
+            Подготовительные задания к курсу
+        </title>
+        <meta name="description" content="Chartist.html">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, minimal-ui">
+        <link id="vendorsbundle" rel="stylesheet" media="screen, print" href="/tasks/css/vendors.bundle.css">
+        <link id="appbundle" rel="stylesheet" media="screen, print" href="/tasks/css/app.bundle.css">
+        <link id="myskin" rel="stylesheet" media="screen, print" href="/tasks/css/skins/skin-master.css">
+        <link rel="stylesheet" media="screen, print" href="/tasks/css/statistics/chartist/chartist.css">
+        <link rel="stylesheet" media="screen, print" href="/tasks/css/miscellaneous/lightgallery/lightgallery.bundle.css">
+        <link rel="stylesheet" media="screen, print" href="/tasks/css/fa-solid.css">
+        <link rel="stylesheet" media="screen, print" href="/tasks/css/fa-brands.css">
+        <link rel="stylesheet" media="screen, print" href="/tasks/css/fa-regular.css">
+    </head>
---
+++ tasks/task_1.php
@@ -0,0 +1,88 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/header.php'?>
+    <body class="mod-bg-1 mod-nav-link ">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="/tasks/task_1.php">Task 1</a>
+                </li>
+            </ul>
+            </nav>
+        </div>
+        <main id="js-page-content" role="main" class="page-content">
+            <div class="container">
+            <div class="col-md-6">
+                <div id="panel-1" class="panel">
+                    <div class="panel-hdr">
+                        <h2>
+                            Задание
+                        </h2>
+                        <div class="panel-toolbar">
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-collapse" data-toggle="tooltip" data-offset="0,10" data-original-title="Collapse"></button>
+                            <button class="btn btn-panel waves-effect waves-themed" data-action="panel-fullscreen" data-toggle="tooltip" data-offset="0,10" data-original-title="Fullscreen"></button>
+                        </div>
+                    </div>
+                    <div class="panel-container show">
+                        <div class="panel-content">
+                            <div class="panel-content">
+                                <div class="bg-warning-100 border border-warning rounded">
+                                    <div class="input-group p-2 mb-0">
+                                        <input type="text" class="form-control form-control-lg shadow-inset-2 bg-warning-50 border-warning" id="js-list-msg-filter" placeholder="Filter list">
+                                        <div class="input-group-append">
+                                            <div class="input-group-text bg-warning-500 border-warning">
+                                                <i class="fal fa-search fs-xl"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php 
+                                        $items = [
+                                            'reports' => [
+                                                'title' => 'Reports',
+                                                'tags' => 'reports file'
+                                            ],
+                                            'analytics' => [
+                                                'title' => 'Analytics',
+                                                'tags' => 'analytics graphs'
+                                            ],
+                                            'export' => [
+                                                'title' => 'Export',
+                                                'tags' => 'export download'
+                                            ],
+                                            'storage' => [
+                                                'title' => 'Storage',
+                                                'tags' => 'storage'
+                                            ]
+                                        ];
+                                    ?>
+                                    <ul id="js-list-msg" class="list-group px-2 pb-2 js-list-filter">
+                                        <?php foreach ($items as $item):?>
+                                            <li class="list-group-item">
+                                                <span data-filter-tags="<?php echo $item['tags']?>"><?php echo $item['title']?></span>
+                                            </li>
+                                        <?php endforeach;?>
+                                        
+                                    </ul>
+                                    <div class="filter-message js-filter-message mt-0 fs-sm"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+            
+        </main>
+
+        <script src="js/vendors.bundle.js"></script>
+        <script src="js/app.bundle.js"></script>
+        <script>
+            // default list filter
+            initApp.listFilter($('#js_default_list'), $('#js_default_list_filter'));
+            // custom response message
+            initApp.listFilter($('#js-list-msg'), $('#js-list-msg-filter'));
+        </script>
+    </body>
+</html>
